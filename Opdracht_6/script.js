bd243dd157548e6a67be5fa2e979a8b88504d69b
abookhoardersobsession <84335181+abookhoardersobsession@users.noreply.github.com>
2021-07-03 21:43:53 +0200

    Add files via upload

---
+++ Opdracht_6/script.js
@@ -0,0 +1,23 @@
+// Step 1
+const makePizza = function() {
+  console.log("Roll dough");
+  console.log("Spread tomato sauce");
+  console.log("Grate cheese");
+  console.log("Bake in oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
+
+// Step 2
+const makeSushi = function() {
+  console.log("Boil rice");
+  console.log("Cut avocado and fish");
+  console.log("Roll sushi");
+};
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
